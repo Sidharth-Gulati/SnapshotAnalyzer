259a560e868d57c7dc226ddc9417e78a57a904fd
Sidharth-Gulati <gulatisidharth107@gmail.com>
2019-09-09 07:05:06 +0530

    Added Listing, Stopping, Starting and Termination commands to the script.

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ Eg : aws configure --profile Your_Name
 # Running
 
 Run the program using the command 'python shots.py' from the directory.
+
+The --uni tag is optinal
+To use the Listing command: 'python shots.py list --uni=<Value>'
+To use the Stopping command : 'python shots.py stop --uni=<Value>'
+To use the Starting command : 'python shots.py start --uni=<Value>'
+To use the Termination command : 'python shots.py terminate --uni=<Value>'
--- Shots/shots.py
+++ Shots/shots.py
@@ -1,21 +1,86 @@
 import boto3
 import click
-session = boto3.Session(profile_name='Sidharth_Gulati')
-ec2 = session.resource('ec2')
+
+session = boto3.Session(profile_name="Sidharth_Gulati")
+ec2 = session.resource("ec2")
 
 
-@click.command()
-def list_instaces():
+def filter_instances(uni):
+    instances = []
+
+    if uni:
+        filters = [{"Name": "tag:Universe", "Values": [uni]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+
+@click.group()
+def instances():
+    "Commands for listing"
+
+
+@instances.command("list")
+@click.option("--uni", default=None, help="Only instances of Universe")
+def list_instances(uni):
     "List EC2 instances"
-    for i in ec2.instances.all():
-        print(', '.join((
-            i.id,
-            i.instance_type,
-            i.placement['AvailabilityZone'],
-            i.state['Name'],
-            i.public_dns_name)))
+
+    instances = filter(uni)
+
+    for i in instances:
+        tags = {t["Key"]: t["Value"] for t in i.tags or []}
+        print(
+            ", ".join(
+                (
+                    i.id,
+                    i.instance_type,
+                    i.placement["AvailabilityZone"],
+                    i.state["Name"],
+                    i.public_dns_name,
+                    tags.get("Universe", "<No Universe>"),
+                )
+            )
+        )
     return
 
 
-if __name__ == '__main__':
-    list_instaces()
+@instances.command("stop")
+@click.option("--uni", default=None, help="Only instances of Universe")
+def stop_instances(uni):
+    "Stop EC2 instances"
+
+    instances = filter_instances(uni)
+
+    for i in instances:
+        print("Stopping {} instance ".format(i.id))
+        i.stop()
+
+
+@instances.command("start")
+@click.option("--uni", default=None, help="Only instances of Universe")
+def start_instances(uni):
+    "Starting EC2 instances"
+
+    instances = filter_instances(uni)
+
+    for i in instances:
+        print("Starting {} instance".format(i.id))
+        i.start()
+
+
+@instances.command("terminate")
+@click.option("--uni", default=None, help="Only instances of Universe")
+def terminate_instances(uni):
+    "Terminate EC2 instances"
+
+    instances = filter_instances(uni)
+
+    for i in instances:
+        print("Terminating {} instance".format(i.id))
+        i.terminate()
+
+
+if __name__ == "__main__":
+    instances()
