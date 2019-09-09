1b6320d21192ee94eec88c9921524d17c98eb014
Sidharth-Gulati <gulatisidharth107@gmail.com>
2019-09-10 02:40:30 +0530

    Setup.py to allow building whl package.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="SnapshotAnalyzer",
+    version="1.0",
+    author="Sidharth Gulati",
+    author_email="gulatisidharth107@gmail.com",
+    description="SnapshotAnalyzer manages EC2 instances, volumes and snapshots",
+    license="GPLv3+",
+    packages=["Shots"],
+    url="https://github.com/Sidharth-Gulati/SnapshotAnalyzer",
+    install_requires=["boto3", "click"],
+    entry_points='''
+    [console_scripts]
+    shots=Shots.shots:cli''',
+)
+
