6a42ac611d635c2dc407277b5bed1ac493a04e0c
Sidharth-Gulati <gulatisidharth107@gmail.com>
2019-09-10 01:24:34 +0530

    Subcommands for EBS Volumes and Snapshots - Base Implement

--- README.md
+++ README.md
@@ -18,10 +18,28 @@ Eg : aws configure --profile Your_Name
 
 # Running
 
+General Pattern -- python shots.py <command> <sub-command>
+
 Run the program using the command 'python shots.py' from the directory.
 
 The --uni tag is optinal
-To use the Listing command: 'python shots.py list --uni=<Value>'
-To use the Stopping command : 'python shots.py stop --uni=<Value>'
-To use the Starting command : 'python shots.py start --uni=<Value>'
-To use the Termination command : 'python shots.py terminate --uni=<Value>'
+
+## For EC2 instances command--- 
+
+To use the Listing sub-command : 'python shots.py instances list --uni=<Value>'
+
+To use the Stopping sub-command : 'python shots.py instances stop --uni=<Value>'
+
+To use the Starting sub-command : 'python shots.py instances start --uni=<Value>'
+
+To use the Termination sub-command : 'python shots.py instances terminate --uni=<Value>'
+
+To use the Create Snapshots sub-command : 'python shots.py'
+
+## For EBS volumes command---
+
+To use the Listing sub-command : 'python shots.py volumes list --uni=<Value>'
+
+## For EC2 snapshots command---
+
+To use the Listing sub-command : 'python shots.py snapshots list --uni=<Value>'
--- Shots/shots.py
+++ Shots/shots.py
@@ -18,16 +18,78 @@ def filter_instances(uni):
 
 
 @click.group()
+def cli():
+    "Shots manages EC2 instances, volumes and snapshots"
+
+
+@cli.group("snapshots")
+def snapshots():
+    """Commands for Snapshots"""
+
+
+@snapshots.command("list", help="List snapshots")
+@click.option("--uni", default=None, help="Only snapshots of Universe")
+def list_snapshots(uni):
+    "List EC2 snapshots"
+
+    instances = filter_instances(uni)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(
+                    ", ".join(
+                        (
+                            s.id,
+                            v.id,
+                            i.id,
+                            s.state,
+                            s.progress,
+                            s.start_time.strftime("%c"),
+                        )
+                    )
+                )
+    return
+
+
+@cli.group("volumes")
+def volumes():
+    """Commands for EBS volumes"""
+
+
+@volumes.command("list", help="List Volumes")
+@click.option("--uni", default=None, help="Only volumes of Universe")
+def list_volumes(uni):
+    instances = filter_instances(uni)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(
+                ", ".join(
+                    (
+                        v.id,
+                        i.id,
+                        v.state,
+                        str(v.size) + "GiB",
+                        v.encrypted and "Encrypted" or "Not Encrypted",
+                    )
+                )
+            )
+
+    return
+
+
+@cli.group("instances")
 def instances():
-    "Commands for listing"
+    """Commands for EC2 instances"""
 
 
-@instances.command("list")
+@instances.command("list", help="List EC2 instances")
 @click.option("--uni", default=None, help="Only instances of Universe")
 def list_instances(uni):
     "List EC2 instances"
 
-    instances = filter(uni)
+    instances = filter_instances(uni)
 
     for i in instances:
         tags = {t["Key"]: t["Value"] for t in i.tags or []}
@@ -46,7 +108,7 @@ def list_instances(uni):
     return
 
 
-@instances.command("stop")
+@instances.command("stop", help="Stop EC2 instances")
 @click.option("--uni", default=None, help="Only instances of Universe")
 def stop_instances(uni):
     "Stop EC2 instances"
@@ -58,7 +120,7 @@ def stop_instances(uni):
         i.stop()
 
 
-@instances.command("start")
+@instances.command("start", help="Start EC2 instances")
 @click.option("--uni", default=None, help="Only instances of Universe")
 def start_instances(uni):
     "Starting EC2 instances"
@@ -70,7 +132,7 @@ def start_instances(uni):
         i.start()
 
 
-@instances.command("terminate")
+@instances.command("terminate", help="Terminate EC2 instances")
 @click.option("--uni", default=None, help="Only instances of Universe")
 def terminate_instances(uni):
     "Terminate EC2 instances"
@@ -82,5 +144,18 @@ def terminate_instances(uni):
         i.terminate()
 
 
+@instances.command("create_snapshots", help="Create Snapshots of EC2 instances")
+@click.option("--uni", default=None, help="Only instances for Universe")
+def create_snapshots(uni):
+    "Create snapshots for all EC2 instances"
+
+    instances = filter_instances(uni)
+
+    for i in instances:
+        i.stop()
+        for v in i.volumes.all():
+            v.create_snapshots(Description="Created by Python Script")
+
+
 if __name__ == "__main__":
-    instances()
+    cli()
